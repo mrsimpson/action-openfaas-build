60525dcdf1b28f70a063b86d9dc478a044b0376a
mrsimpson <github@beimir.net>
2020-03-04 00:04:43 +0100

    buildkit

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,10 +50,7 @@ for BUILDING_MESSAGE in "${BUILDING_MESSAGES[@]}"; do
 
     # Build and push
     cd "${FOLDER}"
-    docker buildx create --driver docker-container --name my-builder
-    docker buildx use my-builder
-    docker buildx install
-    docker build --platform $4 -t $IMAGE_FULL --push .
+    DOCKER_BUILDKIT=1 docker build --platform $4 -t $IMAGE_FULL --push .
     cd -
 
     ((INDEX++))
